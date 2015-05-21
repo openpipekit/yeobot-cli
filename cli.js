e0126f095897f414f01e101a80d24840343c2bee
R.J. Steinert <rj@rjsteinert.com>
2015-05-21 17:45:59 -0400

    Rename to yeobot and change up commands

--- cli.js
+++ cli.js
@@ -8,27 +8,57 @@ var fs = require('fs')
 var replace = require('replace')
 var exec = require('child_process').exec;
 
+var generator = {
+  repository: '',
+  path: '',
+  statement: '',
+  variables: []
+}
+
 program
   .version('0.0.0')
-  .option('-r, --repository <URI>', 'GIT URI pointing to the generator')
+  .arguments('<repository> [destination]')
   .option('-v, --verbose')
-  .option('-k, --keep-git-repo', 'If left out, the .git folder will be cleaned from the generated folder')
-  .parse(process.argv);
+  .option('-k, --keep-git-repo', 'If used the .git folder will not be cleaned from the generated folder')
+  .action(function (repository, destination) {
+    repositoryValue = repository;
+    destinationValue = destination;
+  });
 
-var fetchGen = function(repository, callback) { 
-  var path = process.cwd() + '/autorun'
-  exec('git clone ' + repository + ' ' + path, function(err, stderr, stdout) {
+program.parse(process.argv);
+
+// Check for required repository value
+if (typeof repositoryValue === 'undefined') {
+   console.error('no repository given');
+   process.exit(1);
+}
+else {
+  generator.repository = repositoryValue
+}
+
+// Determine path
+if (typeof destinationValue === 'undefined') {
+  generator.path = process.cwd()
+}
+else if (destinationValue.substr(0, 1) === '/') {
+  generator.path = destinationValue 
+} 
+else if (destinationValue.substr(0, 1) === '.') {
+  generator.path = process.cwd() + destinationValue.substr(1, destinationValue.length) 
+}
+else { 
+  generator.path = process.cwd() + '/' + destinationValue
+}
+
+var fetchGen = function(callback) { 
+  exec('git clone ' + generator.repository + ' ' + generator.path, function(err, stderr, stdout) {
     if (err) return callback(err)
-    callback(null, path) 
+    callback(null) 
   })
 }
 
-var parseGen = function(path, callback) {
-  var generator = {
-    statement: '',
-    variables: []
-  }
-  fs.readFile(path + '/README.md', 'utf8', function (err,data) {
+var parseGen = function(callback) {
+  fs.readFile(generator.path + '/README.md', 'utf8', function (err,data) {
     if (err) return callback(err)
     var lines = data.split('\n')
     var read = false
@@ -38,7 +68,7 @@ var parseGen = function(path, callback) {
       if (read == false && line == '## Statement')  read = true
     }) 
     generator.variables = (generator.statement).match(/{{(.*?)}}/g)
-    callback(null, generator)
+    callback(null)
   });
 }
 
@@ -69,21 +99,22 @@ var recursiveFindAndReplace = function(variables, path, callback) {
       regex: key,
       replacement: value,
       paths: [path],
-      recursive: true
+      recursive: true,
+      silent: true
     })
   })
 }
 
 
 // GO!
-fetchGen(program.repository, function(err, path) {
+fetchGen(function(err) {
   if (err) return console.log(err)
-  parseGen(path, function(err, gen) {
+  parseGen(function(err, gen) {
     if (err) return console.log(err)
-    console.log(gen.statement)
-    var questions = generateQuestions(gen.variables)
+    console.log(generator.statement)
+    var questions = generateQuestions(generator.variables)
     inquirer.prompt(questions, function(answers) {
-      recursiveFindAndReplace(answers, path, function(err) {
+      recursiveFindAndReplace(answers, generator.path, function(err) {
         if (err) return console.log(err)
         console.log('Done.')
       })
